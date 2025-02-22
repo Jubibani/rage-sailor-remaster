54d5b39d7e5386b67805c24046938f2ad74ff556
Jubibani <strawberryloli3@gmail.com>
2025-02-23 03:51:54 +0800

    refactor(boatController): fix horizontal movement

--- Assets/Scripts/BoatPlayer.cs
+++ Assets/Scripts/BoatPlayer.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 public class BoatPlayer : MonoBehaviour {
  
 
-    private CharacterController controller;
+    private CharacterController boatController;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
 
-    private float playerSpeed = 2.0f;
-    private float jumpHeight = 1.0f;
+    private float playerSpeed = 10.0f;
+    private float jumpHeight = 20.0f;
     private float gravityValue = -9.81f;
 
 
@@ -17,27 +17,26 @@ public class BoatPlayer : MonoBehaviour {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        controller = gameObject.AddComponent<CharacterController>();
+        boatController = GetComponent<CharacterController>();
+
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        groundedPlayer = controller.isGrounded;
+        groundedPlayer = boatController.isGrounded;
 
         if (groundedPlayer && playerVelocity.y < 0)
         {
-            playerVelocity.y = 0f;
+            playerVelocity.y = -2f;
+            
         }
+        Debug.Log("Is grounded: " + boatController.isGrounded);
 
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
+        boatController.Move(move * Time.deltaTime * playerSpeed);
 
         //player jump
         if (Input.GetButtonDown("Jump") && groundedPlayer)
@@ -45,8 +44,9 @@ public class BoatPlayer : MonoBehaviour {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
         }
 
+        //Gravity
         playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+        boatController.Move(playerVelocity * Time.deltaTime);
 
     }
 }
